737a283d1911f9eeea2f5d59706d91eacce012e5
i1ya97 <31565819+i1ya97@users.noreply.github.com>
2025-01-12 22:22:16 +0700

    update primary key

--- backend/ApplicationContext.cs
+++ backend/ApplicationContext.cs
@@ -8,7 +8,7 @@ public class ApplicationContext: DbContext
 
     public DbSet<Product> Products { get; set; } = null!;
 
-    public DbSet<Log> UploadLogs { get; set; } = null!;
+    public DbSet<Entity.Log> UploadLogs { get; set; } = null!;
 
     public DbSet<Stock> Stoks { get; set; } = null!;
 
@@ -16,4 +16,9 @@ public class ApplicationContext: DbContext
 
     public ApplicationContext(DbContextOptions<ApplicationContext> options)
     : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Entity.Log>().HasKey(u => new { u.Id, u.User_id });
+    }
 }
--- backend/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a5002fcbb5e31a14167e5e16f383d1a3304670")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a18d53577bfe35afa481f2a9ee7cdecd6b3b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
